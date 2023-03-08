dc83cd0aced8afdff7ea11741066713223f936d7
kot3u <125024243+kot3u@users.noreply.github.com>
2023-03-08 13:16:46 +0100

    Added  password matching check

---
+++ app.js
@@ -0,0 +1,31 @@
+const password = document.querySelector('#password')
+const passwordConfirm = document.querySelector('#password-confirm')
+const output = document.querySelector('#password-match')
+const button = document.querySelector('.singup-button')
+let matching
+
+function checkPassword () {
+  if (password.value === passwordConfirm.value){
+    output.textContent = 'Passwords do match!';
+    output.className = 'good';
+    button.disabled = false;
+  }
+  else {
+    output.textContent = 'Passwords don\'t match';
+    output.className = 'bad';
+    button.disabled = true;
+  };
+};
+
+password.addEventListener('keyup', () => {
+  if(passwordConfirm.value){
+    checkPassword();
+    
+  };
+});
+
+passwordConfirm.addEventListener('keyup', () => {
+  if (password.value) {
+    checkPassword();
+  };
+});
