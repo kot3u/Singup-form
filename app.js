4beb00604d6abe53793d8be5edff699816b042be
kot3u <25024243+kot3u@users.noreply.github.com>
2023-03-12 18:36:46 +0100

    prettier code

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
-const password = document.querySelector('#password')
-const passwordConfirm = document.querySelector('#password-confirm')
-const output = document.querySelector('#password-match')
-const button = document.querySelector('.singup-button')
-let matching
+const password = document.querySelector("#password");
+const passwordConfirm = document.querySelector("#password-confirm");
+const output = document.querySelector("#password-match");
+const button = document.querySelector(".singup-button");
+let matching;
 
-function checkPassword () {
-  if (password.value === passwordConfirm.value){
-    output.textContent = 'Passwords do match!';
-    output.className = 'good';
+function checkPassword() {
+  if (password.value === passwordConfirm.value) {
+    output.textContent = "Passwords do match!";
+    output.className = "good";
     button.disabled = false;
-  }
-  else {
-    output.textContent = 'Passwords don\'t match';
-    output.className = 'bad';
+  } else {
+    output.textContent = "Passwords don't match";
+    output.className = "bad";
     button.disabled = true;
-  };
-};
+  }
+}
 
-password.addEventListener('keyup', () => {
-  if(passwordConfirm.value){
+password.addEventListener("keyup", () => {
+  if (passwordConfirm.value) {
     checkPassword();
-    
-  };
+  }
 });
 
-passwordConfirm.addEventListener('keyup', () => {
+passwordConfirm.addEventListener("keyup", () => {
   if (password.value) {
     checkPassword();
-  };
+  }
 });
